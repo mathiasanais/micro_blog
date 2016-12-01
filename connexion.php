1f63d59bb90b803c6c1df79f0dda045a41bce967
Anaïs Mathias <Anas Mathias>
2016-12-01 13:53:22 +0100

    Ajout supp modif date

--- connexion.php
+++ connexion.php
@@ -1,15 +1,28 @@
 <?php
 include('includes/connexion.inc.php');
 include('includes/haut.inc.php');
+
+if(isset($_POST['email'])&&!empty($_POST['email']) && isset($_POST['mdp'])&&!empty($_POST['mdp']))
+{
+	
+}
+else
+{
+	
 ?>
-<form>
+<form method="POST">
   <div class="form-group col-md-7">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" name="email" id="email" placeholder="Email">
   </div>
   <div class="form-group col-md-7">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="mdp">Password</label>
+    <input type="password" class="form-control" name ="mdp" id="mdp" placeholder="Password">
   </div>
   <button type="submit" class="btn btn-default col-md-2">Submit</button>
 </form>
+<?php
+}
+
+
+?>
